5da0b6167d7e989d35df9a8de27a28a73c759296
claudiaacampos <claudiaa.campos@softtek.com>
2024-09-25 18:25:30 +0200

    Styles update

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,11 @@
 </template>
 
 <style>
-html, body {
+html,
+body {
   margin: 0;
   padding: 0;
-} 
+}
 
 p {
   margin: 0;
@@ -26,7 +27,7 @@ p {
   color: #2c3e50;
   display: flex;
   flex-direction: column;
-  gap: 2rem;
+  gap: 3rem;
   margin: 0;
   padding: 0;
 }
@@ -47,23 +48,23 @@ nav a.router-link-exact-active {
   color: #42b883;
 }
 
-.container {
+.todo-container {
   display: flex;
-  gap: 3rem;
+  gap: 4rem;
   padding: 2rem;
+  border-top: thin solid #cccccc;
+  border-bottom: thin solid #cccccc;
+  box-shadow: 0px 5px 15px -15px;
 }
 
-.add-container {
+.logo-container {
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
-  gap: 1;
-  max-width: 50%;
-  max-height: min-content;
+  gap: 0.75rem;
+  width: 40%;
 }
 
 .logo {
-  height: 30%;
   width: 30%;
   margin: 0 auto;
 }
@@ -73,7 +74,7 @@ h1 {
   margin: 0;
 }
 
-.todo-bar {
+.todo-add-bar {
   display: flex;
   align-items: center;
   justify-content: center;
@@ -83,25 +84,23 @@ h1 {
 
 input {
   border: 1px solid #42b883;
-  /* border-right: 0;
-  border-top-left-radius: 40px;
-  border-bottom-left-radius: 40px; */
   border-radius: 40px;
-  padding: 0 13.5rem 0 2.5rem;
-  height: 65px;
+  padding: 0 9.5rem 0 2rem;
+  height: 60px;
   align-items: center;
+  justify-content: flex-end;
   margin: 0 auto;
   max-width: 100%;
-  height: 85px;
   flex-grow: 2;
   position: relative;
   box-sizing: border-box;
   outline: 0;
-  font-size: 1.75rem;
+  font-size: 1rem;
 }
+
 input::placeholder {
-  font-size: 1.75rem;
-  line-height: 1.4;
+  font-size: 1.125rem;
+  line-height: 1.4rem;
 }
 
 input:hover {
@@ -112,50 +111,54 @@ input:hover {
 .add-btn {
   border-top-right-radius: 40px;
   border-bottom-right-radius: 40px;
-  border: 1px solid #42b883; 
+  border: 1px solid #42b883;
   background-color: #42b883;
-  color: #FFFFFF;
+  color: #ffffff;
+  font-size: 1.75rem;
+  font-weight: 600;
+  text-transform: uppercase;
   z-index: 1;
-  padding: 0.5rem 2rem;
   cursor: pointer;
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 85px;
-  
   position: absolute;
   right: 0;
-  font-size: 1.75rem;
-  font-weight: 600;
-  text-transform: uppercase;
   gap: 0.75rem;
-  width: 40%;
+  height: 60px;
+  width: 20%;
 }
 
 .add-btn:hover {
   background-color: #2c3e50;
-  color: #FFFFFF;
+  color: #ffffff;
 }
 
-.list-container {
+.todo-add-container {
   display: flex;
   flex-direction: column;
-  gap: 1.5rem;
-  padding: 2rem;
-  max-width: 40%;
+  gap: 2rem;
+  width: 50%;
 }
 
 h2 {
   margin: 0;
 }
 
-ul {
+.list-container {
   display: flex;
   flex-direction: column;
-  gap: 1.1rem;
+  padding: 3rem 3rem 6rem;
+  gap: 2rem;
+}
+
+ul {
+  display: grid;
+  grid-template-columns: auto auto;
+  gap: 1.5rem;
   list-style: none;
   padding: 0;
-  margin: 20px 0 0;
+  margin: 0;
 }
 
 li {
@@ -163,32 +166,49 @@ li {
   align-items: center;
   justify-content: space-between;
   border: none;
-  max-width: 50%; 
+  text-transform: capitalize;
+  font-size: 1.5rem;
+  gap: 2rem;
+  min-width: 100%;
 }
 
 li span {
   font-size: 1.75rem;
-  text-transform: capitalize;
+  line-height: 0.75rem;
+  font-weight: bolder;
+  color: #42b883;
+  margin: 0 0.5rem 0 0;
 }
-.btn-remove {
-  border: 0;
-  outline: 0;
-  background-color: transparent;
-}
+
 .remove-btn {
-  background-color: #FFFFFF;
-  fill: #CCCCCC;
+  background-color: #ffffff;
+  fill: #42b883;
   border: 0;
   border-radius: 50%;
   display: flex;
   align-items: center;
   justify-content: center;
   font-size: 1.5rem;
-  width: 2.3rem;
-  height: 2.3rem;
+  width: 2rem;
+  height: 2rem;
 }
 .remove-btn:hover {
   transform: scale(1.3);
   fill: #42b883;
+}
+
+.send-list {
+  display: flex;
+  align-content: center;
+}
+.send-form {
+  display: flex;
+  align-items: center;
+}
+.send-btn {
+  background-color: #42b883;
+  outline: none;
+  border: 0;
+  min-height: 60px;
 }
 </style>
--- src/views/Composition.vue
+++ src/views/Composition.vue
@@ -1,59 +1,63 @@
 <template>
-  <div class="container">
-    <div class="add-container">
+  <div class="todo-container">
+    <div class="logo-container">
       <img alt="Vue logo" src="../assets/logo.png" class="logo" />
       <h1>Todo's List!</h1>
       <p>To start, add items to your list.</p>
-      <!-- <div class="todo-bar">
-        <input
-         v-model="data.newTodoName" 
-         @keyup.enter="addTodo" 
-         placeholder="Add a Todo" 
-         type="text" 
-         minlength="2"
-         maxlength="250"
-        />
-        <button @click="addTodo(index)" class="add-btn">
-          <svg width="40px" height="40px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path d="M6 12H18M12 6V18" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-          </svg>
-        </button>
-      </div> -->
     </div>
 
-    <div class="list-container">
-      <h2 v-if="todosCount >= 1">You have {{ todosCount }} Todos!</h2>
-      <h2 v-else>Add your Todo's!</h2>
+    <div class="todo-add-container">
+      <h2>Add Todo's to your list!</h2>
+      <p>To start, add items to your list.</p>
 
-      <div class="todo-bar">
-        <input
-         v-model="data.newTodoName" 
-         @keyup.enter="addTodo" 
-         placeholder="Add a Todo" 
-         type="text" 
-         minlength="2"
-         maxlength="250"
-        />
+      <div class="todo-add-bar">
+        <input v-model="data.newTodoName" @keyup.enter="addTodo" placeholder="Add a Todo" type="text" minlength="2" maxlength="250" />
         <button @click="addTodo(index)" class="add-btn">
           <svg width="40px" height="40px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path d="M6 12H18M12 6V18" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
           </svg>
         </button>
       </div>
+      <p>{{ characters }} number of characters left.</p>
+    </div>
+  </div>
 
-      <ul>
-        <li v-for="(todo, index) in data.todos" :key="todo.id">
-          <span>· </span>
-          <span>{{ todo.name }}</span>
-          <button @click="deleteTodo(index)" class="remove-btn">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-              <path
-                d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0L284.2 0c12.1 0 23.2 6.8 28.6 17.7L320 32l96 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 96C14.3 96 0 81.7 0 64S14.3 32 32 32l96 0 7.2-14.3zM32 128l384 0 0 320c0 35.3-28.7 64-64 64L96 512c-35.3 0-64-28.7-64-64l0-320zm96 64c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16z"
-              />
-            </svg>
-          </button>
-        </li>
-      </ul>
+  <div class="list-container">
+    <h2 v-if="todosCount >= 1">You have {{ todosCount }} Todos!</h2>
+
+    <ul>
+      <li v-for="(todo, index) in data.todos" :key="todo.id">
+        <p>
+          <span>- </span>
+          {{ todo.name }}
+        </p>
+        <button @click="deleteTodo(index)" class="remove-btn">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+            <path
+              d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0L284.2 0c12.1 0 23.2 6.8 28.6 17.7L320 32l96 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 96C14.3 96 0 81.7 0 64S14.3 32 32 32l96 0 7.2-14.3zM32 128l384 0 0 320c0 35.3-28.7 64-64 64L96 512c-35.3 0-64-28.7-64-64l0-320zm96 64c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16z"
+            />
+          </svg>
+        </button>
+      </li>
+    </ul>
+
+    <div class="send-list">
+      <h3>Send your list to your phone</h3>
+
+      <div class="send-form">
+        <input type="text" />
+        <input type="text" />
+        <button @click="" class="send-btn">
+          <svg width="40px" height="40px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M3.3938 2.20468C3.70395 1.96828 4.12324 1.93374 4.4679 2.1162L21.4679 11.1162C21.7953 11.2895 22 11.6296 22 12C22 12.3704 21.7953 12.7105 21.4679 12.8838L4.4679 21.8838C4.12324 22.0662 3.70395 22.0317 3.3938 21.7953C3.08365 21.5589 2.93922 21.1637 3.02382 20.7831L4.97561 12L3.02382 3.21692C2.93922 2.83623 3.08365 2.44109 3.3938 2.20468ZM6.80218 13L5.44596 19.103L16.9739 13H6.80218ZM16.9739 11H6.80218L5.44596 4.89699L16.9739 11Z"
+              fill="#FFFFFF"
+            />
+          </svg>
+        </button>
+      </div>
     </div>
   </div>
 </template>
